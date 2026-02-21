8c33c2c31fbd1b46f70d8fca23100be713408fcc
ellalau58581 <ella.lau.58581@gmail.com>
2026-02-21 01:10:36 -0500

    first print to begin exam calculator

--- Untitled-1.swift
+++ Untitled-1.swift
@@ -1,11 +1,13 @@
-
-let hour  = 10
-if hour < 12 {
-    print("Good Morning!")
-} else if hour < 18 {
-    print("Good Afternoon!")
-} else if hour < 21 {
-    print("Good Evening!") 
- } else {
-    print("Good Night") 
+print("Enter the number of minutes you study in a day for calculus:")
+if let input = readLine(), let minutes = Int(input) {
+    let hours = minutes / 60
+    let percentage = min(100, ((hours / 10) + 1) * 10)
+    let random = Int.random(in: 0..<100)
+    if random < percentage {
+        print("You will pass the calculus exam!")
+    } else {
+        print("You will not pass the calculus exam.")
     }
+} else {
+    print("Invalid input.")
+}
