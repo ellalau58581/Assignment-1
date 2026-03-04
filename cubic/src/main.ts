274c05d6b3bfb2832bc710a66e37341631c27a22
ellalau58581 <ella.lau.58581@gmail.com>
2026-03-04 10:24:25 -0500

    made changes

---
+++ cubic/src/main.ts
@@ -0,0 +1,12 @@
+import './style.css'
+const form = document.querySelector('form') as HTMLFormElement
+const input = document.querySelector('input') as HTMLInputElement
+const result = document.querySelector('#result') as HTMLDivElement
+
+const a = document.querySelector('#a') as HTMLInputElement
+const b = document.querySelector('#b') as HTMLInputElement
+const c = document.querySelector('#c') as HTMLInputElement
+const d = document.querySelector('#d') as  HTMLInputElement
+const p = (3*a*c - b*b) / (3*a*a)
+const q = (27a**3*d - 9*a*b*c + 2*b**3) / (27*a**3)
+
