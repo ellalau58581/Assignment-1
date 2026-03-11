8701e0501a25ed2351b4160981c3bfe16ea5b7a4
ellalau58581 <ella.lau.58581@gmail.com>
2026-03-10 22:47:14 -0400

    fixed a bug

--- cubic/src/main.ts
+++ cubic/src/main.ts
@@ -25,48 +25,38 @@ form?.addEventListener("submit", (event) => {
   const q = (27 * a * a * d - 9 * a * b * c + 2 * b ** 3) / (27 * a ** 3);
   const deltaSymbol = (q / 2) ** 2 + (p / 3) ** 3;
   document.getElementById("q-result")!.textContent = `${q}`;
+  document.getElementById("p-result")!.textContent = `${p}`;
   document.getElementById("deltaSymbol-result")!.textContent = `${deltaSymbol}`;
 
-  const x1Result = document.getElementById("x1-result") as HTMLParagraphElement;
-  const x2Result = document.getElementById("x2-result") as HTMLParagraphElement;
-  const x3Result = document.getElementById("x3-result") as HTMLParagraphElement;
-  const eplison = 1e-12;
-  if (deltaSymbol < -eplison) {
+  if (deltaSymbol < 0) {
     const theta = Math.acos(Math.min(Math.max(-q / (2 * Math.sqrt((-(p / 3)) ** 3)), -1), 1)) / 3;
     const x1 = 2 * Math.sqrt(-p / 3) * Math.cos(theta) - b / (3 * a);
-    const x2 = 2 fi* Math.sqrt(-p / 3) * Math.cos(theta + (2 * Math.PI / 3)) - b / (3 * a);
+    const x2 = 2 * Math.sqrt(-p / 3) * Math.cos(theta + (2 * Math.PI / 3)) - b / (3 * a);
     const x3 = 2 * Math.sqrt(-p / 3) * Math.cos(theta + (4 * Math.PI / 3)) - b / (3 * a);
-    x1Result.textContent = roundToInteger(x1).toString();
-    x2Result.textContent = roundToInteger(x2).toString();
-    x3Result.textContent = roundToInteger(x3).toString();
+    (document.getElementById("x1-result") as HTMLParagraphElement).textContent = `${x1}`;
+    (document.getElementById("x2-result") as HTMLParagraphElement).textContent = `${x2}`;
+    (document.getElementById("x3-result") as HTMLParagraphElement).textContent = `${x3}`;
 
-  } else if (deltaSymbol > eplison) {
+  } else if (deltaSymbol > 0) {
     const x1 = Math.cbrt(-q / 2 + Math.sqrt(deltaSymbol)) + Math.cbrt(-q / 2 - Math.sqrt(deltaSymbol)) - b / (3 * a);
-    x1Result.textContent = roundToInteger(x1).toString();
-    x2Result.textContent = "Complex";
-    x3Result.textContent = "Complex";
+    (document.getElementById("x1-result") as HTMLParagraphElement).textContent = `${x1}`;
+    (document.getElementById("x2-result") as HTMLParagraphElement).textContent = `${"complex"}`;
+    (document.getElementById("x3-result") as HTMLParagraphElement).textContent = `${"complex"}`;
   } else {
 
-    if (Math.abs(p) < eplison && Math.abs(q) < eplison) {
+    if (q === 0 && p === 0) {
       const x1 = Math.cbrt((-q / 2) + Math.sqrt(deltaSymbol)) + Math.cbrt((-q / 2) - Math.sqrt(deltaSymbol)) - b / (3 * a);
-      x1Result.textContent = roundToInteger(x1).toString();
-      x2Result.textContent = roundToInteger(x1).toString();
-      x3Result.textContent = roundToInteger(x1).toString();
+      (document.getElementById("x1-result") as HTMLParagraphElement).textContent = `${x1}`;
+      (document.getElementById("x2-result") as HTMLParagraphElement).textContent = `${x1}`;
+      (document.getElementById("x3-result") as HTMLParagraphElement).textContent = `${x1}`;
 
     } else {
       const x1 = Math.cbrt((-q / 2) + Math.sqrt(deltaSymbol)) + Math.cbrt((-q / 2) - Math.sqrt(deltaSymbol)) - b / (3 * a); // Cardano
       const x2 = -Math.cbrt(-q / 2) - b / (3 * a); // Single root
-      x1Result.textContent = roundToInteger(x1).toString();
-      x2Result.textContent = roundToInteger(x2).toString();
-      x3Result.textContent = roundToInteger(x2).toString();
+      (document.getElementById("x1-result") as HTMLParagraphElement).textContent = `${x1}`;
+      (document.getElementById("x2-result") as HTMLParagraphElement).textContent = `${x2}`;
+      (document.getElementById("x3-result") as HTMLParagraphElement).textContent = `${x2}`;
 
     }
   }
-  document.getElementById("p-table")!.textContent = p.toFixed(6);
-  document.getElementById("q-table")!.textContent = q.toFixed(6);
-  document.getElementById("discriminant-table")!.textContent = deltaSymbol.toFixed(6);
-  document.getElementById("root1-table")!.textContent = x1.toFixed(6);
-  document.getElementById("root2-table")!.textContent = x2.toFixed(6);
-  document.getElementById("root3-table")!.textContent = x3.toFixed(6);
-  document.getElementById("roots-table")!.style.display = "table";
 });
